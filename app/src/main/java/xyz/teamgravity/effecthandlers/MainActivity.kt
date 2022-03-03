e7834378154287b806fd487c9e56dc3d173d89ef
raheemadamboev <vividricemaryandmax@gmail.com>
2022-03-03 13:22:51 +0500

    initial

---
+++ app/src/main/java/xyz/teamgravity/effecthandlers/MainActivity.kt
@@ -0,0 +1,26 @@
+package xyz.teamgravity.effecthandlers
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import xyz.teamgravity.effecthandlers.ui.theme.EffectHandlersTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            EffectHandlersTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
